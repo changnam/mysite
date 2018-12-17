4ccfd474ca1a3626298361d37fea893befa37dcc
changnam <31008617+changnam@users.noreply.github.com>
2018-12-17 18:41:50 +0900

    rename data file in mount
    rename datafile when database could not ope due to missing file.

--- SQLLIB/datafile.sql
+++ SQLLIB/datafile.sql
@@ -7,3 +7,8 @@ ALTER TABLESPACE sysaux
     RENAME DATAFILE 'C:\APP\ERIC\ORADATA\JERRY\SYSAUX01.DBF'
                  TO 'D:\ORADATA\JERRY\SYSAUX01.DBF';
 alter tablespace sysaux online;
+
+-- down 상태에서 파일이 이동된 경우 변경 방법 (파일 missing 으로 open이 안되고, mount 상태임)
+ALTER DATABASE RENAME FILE 'D:\ORADATA\SYSAUX01.DBF' TO 'E:\ORADATA\SYSAUX01.DBF';
+
+
