277ad3447a7eee4129709116f12f60d09ee108de
test <aa@aa>
2019-09-10 19:11:18 +0900

    git hook logic test

--- hook_installer.py
+++ hook_installer.py
@@ -6,10 +6,18 @@ absPath = os.path.abspath(os.path.dirname(__file__))
 
 sourceFile = absPath + "/hooks/commit-msg"
 targetFile = absPath + "/.git/hooks/commit-msg"
-os.symlink(sourceFile,targetFile)
+try:
+    os.symlink(sourceFile,targetFile)
+    print(f'hook {targetFile} created.')
+except FileExistsError:
+    print(f'hook {targetFile} already exists.')
 
 sourceFile = absPath + "/hooks/pre-commit"
 targetFile = absPath + "/.git/hooks/pre-commit"
-os.symlink(sourceFile,targetFile)
+try:
+    os.symlink(sourceFile,targetFile)
+    print(f'hook {targetFile} created.')
+except FileExistsError:
+    print(f'hook {targetFile} already exists.')
 
 print ("hoook creatation completed.")
--- hooks/pre_commit_test.py
+++ hooks/pre_commit_test.py
@@ -1,9 +1,14 @@
 import subprocess
 # run() returns a CompletedProcess object if it was successful
 # errors in the created process are raised here too
+output = None
 def checkHash():
     process = subprocess.run(['git','rev-parse','HEAD'], check=True, stdout=subprocess.PIPE, universal_newlines=True)
     output = process.stdout
+    print("xxx")
+    process
 
+def branchNameCheck():
+    pass
 if __name__ == "__main__":
     print (output)
