2930d57f4af95b191683cb58987e126f7b403f20
changnam go <changnam.go@gmail.com>
2021-02-13 14:11:43 +0900

    psql

--- SQLLIB/mysql/priv.sql
+++ SQLLIB/mysql/priv.sql
@@ -1,4 +1,5 @@
 select * from information_schema.user_privileges;
 select * from information_schema.user_privileges where grantee like "'root'%";
-grant all privileges on *.* to 'root'@'%' identified by 'P@ssw0rd';
+grant all privileges on *.* to 'root'@'%';
+grant all privileges on keycloak.* to 'keycloak'@'%';
 
--- SQLLIB/postgres/database.sql
+++ SQLLIB/postgres/database.sql
@@ -1,1 +1,11 @@
 select * from pg_database;
+create database keycloak;
+create user keycloak identified by 'keycloak';
+select current_schema;
+create table testtab (id int);
+insert into testtab values(30);
+
+select current_database();
+select current_schema;
+select current_user;
+SELECT SESSION_USER;
